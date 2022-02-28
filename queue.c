d70055ffdb5137b8f7c602034a0e1a70d190ad2f
garrod1227 <garrod1227@gmail.com>
2022-03-01 03:00:38 +0800

    Implement q_sort() and list_qsort()

--- queue.c
+++ queue.c
@@ -274,9 +274,44 @@ void q_reverse(struct list_head *head)
     } while (n != head);
 }
 
+void list_qsort(struct list_head *head)
+{
+    struct list_head list_less, list_greater;
+    element_t *pivot;
+    element_t *item = NULL, *is = NULL;
+
+    if (list_empty(head) || list_is_singular(head))
+        return;
+
+    INIT_LIST_HEAD(&list_less);
+    INIT_LIST_HEAD(&list_greater);
+
+    pivot = list_first_entry(head, element_t, list);
+    list_del(&pivot->list);
+
+    list_for_each_entry_safe (item, is, head, list) {
+        if (strcmp(item->value, pivot->value) <= 0)
+            list_move_tail(&item->list, &list_less);
+        else
+            list_move(&item->list, &list_greater);
+    }
+
+    list_qsort(&list_less);
+    list_qsort(&list_greater);
+
+    list_add(&pivot->list, head);
+    list_splice(&list_less, head);
+    list_splice_tail(&list_greater, head);
+}
+
 /*
  * Sort elements of queue in ascending order
  * No effect if q is NULL or empty. In addition, if q has only one
  * element, do nothing.
  */
-void q_sort(struct list_head *head) {}
+void q_sort(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+    list_qsort(head);
+}
